729f35e8da07e5469d1e76d3567e0015086a1c58
semyon <odintsovvans@gmail.com>
2023-06-09 19:16:19 +0600

    Revert "added title_case func"
    This reverts commit 2770981ac2615a5775476c8d98395d7d01f7a3d6.

--- str_func.py
+++ str_func.py
@@ -1,10 +1,3 @@
 def upper_case(inp):
     """Увеличивет все буквы в строке"""
     return inp.upper()
-
-
-def title_case(inp):
-    """
-    Увеличивет первые буквы слов
-    """
-    return inp.title()
